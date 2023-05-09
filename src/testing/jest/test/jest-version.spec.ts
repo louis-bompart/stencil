0e31dacef485ee5047f9688d912118ff4283139c
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-05-09 14:26:22 -0400

    more tests

--- src/testing/jest/test/jest-version.spec.ts
+++ src/testing/jest/test/jest-version.spec.ts
@@ -1,7 +1,7 @@
 import * as JestFacade from '../jest-facade';
-import { getJestMajorVersion } from '../jest-version';
+import { getJestMajorVersion, getJestRunner, getTestingModuleNames } from '../jest-version';
 
-describe('getVersion', () => {
+describe('jest-utils', () => {
   let getVersionSpy: jest.SpyInstance<
     ReturnType<typeof JestFacade.getVersion>,
     Parameters<typeof JestFacade.getVersion>
@@ -15,8 +15,42 @@ describe('getVersion', () => {
     getVersionSpy.mockRestore();
   });
 
-  it('returns the value returned by getVersion()', () => {
-    getVersionSpy.mockImplementation(() => '28.0.0');
-    expect(getJestMajorVersion()).toBe(28);
+  describe('getVersion', () => {
+    it('returns the value returned by getVersion()', () => {
+      getVersionSpy.mockImplementation(() => '28.0.0');
+      expect(getJestMajorVersion()).toBe(28);
+    });
+  });
+
+  describe('getTestingModuleNames', () => {
+    it.each([
+      ['23.0.0', ['@types/jest', 'jest', 'jest-cli']],
+      ['24.0.0', ['@types/jest', 'jest', 'jest-cli']],
+      ['25.0.0', ['@types/jest', 'jest', 'jest-cli']],
+      ['26.0.0', ['@types/jest', 'jest', 'jest-cli']],
+      ['27.0.0', ['@types/jest', 'jest', 'jest-cli']],
+      ['28.0.0', ['@types/jest', 'jest', 'jest-cli']],
+      ['29.0.0', ['@types/jest', 'jest', 'jest-cli']],
+    ])('returns the correct module names for jest %s', (jestMajorVersion, moduleNames) => {
+      getVersionSpy.mockImplementation(() => jestMajorVersion);
+
+      expect(getTestingModuleNames()).toEqual(moduleNames);
+    });
+  });
+
+  describe('getJestRunner()', () => {
+    it.each([
+      ['23.0.0', 'jest-jasmine2'],
+      ['24.0.0', 'jest-jasmine2'],
+      ['25.0.0', 'jest-jasmine2'],
+      ['26.0.0', 'jest-jasmine2'],
+      ['27.0.0', 'jest-jasmine2'],
+      ['28.0.0', 'jest-circus'],
+      ['29.0.0', 'jest-circus'],
+    ])('returns the correct module names for jest %s', (jestMajorVersion, runnerName) => {
+      getVersionSpy.mockImplementation(() => jestMajorVersion);
+
+      expect(getJestRunner()).toEqual(runnerName);
+    });
   });
 });
