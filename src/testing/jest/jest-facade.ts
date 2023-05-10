d298e0c81923ff54f078114ddafede0edb5f76c4
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-05-10 10:15:18 -0400

    attempt and fail at a facade

--- src/testing/jest/jest-facade.ts
+++ src/testing/jest/jest-facade.ts
@@ -1,10 +1,38 @@
 import { getVersion as jestGetVersion } from 'jest';
 
-/**
- * Get the current major version of Jest that Stencil reconciles
- *
- * @returns the version of Jest.
- */
+// Probably a good bit of over-engineering
+//
+// This assumes we know the version/can resolve jest ourselves
+// There is something to be said about:
+// - Not being able to resolve Jest (at all, not the expected version e.g. monorepo)
+// - Trying to resolve Jest and follow up attempts/strategies
+//
+// Probably the way forward (if at all) is to try to resolve Jest once and cache that decision
+abstract class JestFacade {
+  // eslint-disable-next-line jsdoc/require-returns-check
+  /**
+   * Get the current major version of Jest that Stencil reconciles
+   *
+   * @returns the version of Jest.
+   */
+  static getVersion = (): string => {
+    throw 'not implemented';
+  };
+
+  // @ts-ignore let this be unused
+  someRandoFunction = () => {
+    throw 'not implemented';
+  };
+}
+
+// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class Jest27Facade extends JestFacade {
+  static override getVersion = (): string => {
+    return jestGetVersion();
+  };
+}
+
 export const getVersion = (): string => {
   return jestGetVersion();
 };
