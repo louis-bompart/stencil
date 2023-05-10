028c0f37bee9dad912b95d07d35a227552336a91
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-05-10 10:50:01 -0400

    proto factory for jest-config

--- src/testing/jest/jest-version.ts
+++ src/testing/jest/jest-version.ts
@@ -1,5 +1,9 @@
+import type { Config } from '@jest/types';
+import { ValidatedConfig } from '@stencil/core/declarations';
 import semverMajor from 'semver/functions/major';
 
+import { buildJestArgv as buildJest28Argv } from './experimental/jest-28/jest-config';
+import { buildJestArgv as buildJest27Argv } from './jest-config';
 import { getVersion } from './jest-facade';
 
 // TODO(NOW): Fixup w examples
@@ -30,4 +34,22 @@ export const getJestRunner = (): string => {
     return 'jest-jasmine2';
   }
   return 'jest-circus';
+};
+
+export type JestArgvFactoryOpts = {
+  config: ValidatedConfig;
+};
+export const buildJestArgvFactory = (opts: JestArgvFactoryOpts): Config.Argv => {
+  switch (getJestMajorVersion()) {
+    case 24:
+    case 25:
+    case 26:
+    case 27:
+      return buildJest27Argv(opts.config);
+    case 28:
+      return buildJest28Argv(opts.config);
+    case 29:
+    default:
+      throw new Error(`No config for version!`);
+  }
 };
