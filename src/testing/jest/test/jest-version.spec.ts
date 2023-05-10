1d4a3cfdb131cf1f54b6ae4e44e320a88a54c73d
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-05-10 09:32:13 -0400

    bike shedding tests

--- src/testing/jest/test/jest-version.spec.ts
+++ src/testing/jest/test/jest-version.spec.ts
@@ -16,9 +16,17 @@ describe('jest-utils', () => {
   });
 
   describe('getVersion', () => {
-    it('returns the value returned by getVersion()', () => {
-      getVersionSpy.mockImplementation(() => '28.0.0');
-      expect(getJestMajorVersion()).toBe(28);
+    it.each([
+      ['27.0.0', 27],
+      ['28.1.0', 28],
+      ['29.1.2', 29],
+      ['29.1.2-3', 29],
+      ['29.1.2-alpha.0', 29],
+      ['29.1.2-beta.1', 29],
+      ['29.1.2-rc.2', 29],
+    ])('transforms semver string %s into major version %d', (semverStr, majorVersion) => {
+      getVersionSpy.mockImplementation(() => semverStr);
+      expect(getJestMajorVersion()).toBe(majorVersion);
     });
   });
 
@@ -31,6 +39,7 @@ describe('jest-utils', () => {
       ['27.0.0', ['@types/jest', 'jest', 'jest-cli']],
       ['28.0.0', ['@types/jest', 'jest', 'jest-cli']],
       ['29.0.0', ['@types/jest', 'jest', 'jest-cli']],
+      ['30.0.0', ['@types/jest', 'jest', 'jest-cli']],
     ])('returns the correct module names for jest %s', (jestMajorVersion, moduleNames) => {
       getVersionSpy.mockImplementation(() => jestMajorVersion);
 
@@ -47,6 +56,7 @@ describe('jest-utils', () => {
       ['27.0.0', 'jest-jasmine2'],
       ['28.0.0', 'jest-circus'],
       ['29.0.0', 'jest-circus'],
+      ['30.0.0', 'jest-circus'],
     ])('returns the correct module names for jest %s', (jestMajorVersion, runnerName) => {
       getVersionSpy.mockImplementation(() => jestMajorVersion);
 
